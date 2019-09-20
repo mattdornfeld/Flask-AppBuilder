3a1d5b974223a8eba64aeb210c0bb39a9f9d96dc
Matthew Dornfeld <mdornfe1@gmail.com>
2019-09-20 18:12:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Flask-WTF>=0.14.2,<1',
         'Flask-JWT-Extended>=3.18,<4',
         'python-dateutil>=2.3,<3',
-        'marshmallow>=2.18.0,<2.20',
+        'marshmallow>=2.18.0,<4.0.0',
         'marshmallow-enum>=1.4.1,<2',
         'marshmallow-sqlalchemy>=0.16.1<1',
         'prison==0.1.0',
